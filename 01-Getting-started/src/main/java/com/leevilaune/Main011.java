711f89f180febdf7454ad9591d295da36eed1e1c
leevivl <leevi.laune@metropolia.fi>
2025-08-18 09:44:32 +0300

    add task 2&3

---
+++ 01-Getting-started/src/main/java/com/leevilaune/Main011.java
@@ -0,0 +1,7 @@
+package com.leevilaune;
+
+public class Main011 {
+    public static void main(String[] args) {
+        System.out.println("Greeting with simple message");
+    }
+}
---
+++ 01-Getting-started/src/main/java/com/leevilaune/Main012.java
@@ -0,0 +1,10 @@
+package com.leevilaune;
+
+public class Main012 {
+    public static void main(String[] args){
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+    }
+}
