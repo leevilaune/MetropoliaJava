f9eba1261f9619503b7a3a5d29cf8440a75add03
leevivl <leevi.laune@metropolia.fi>
2025-09-30 10:33:16 +0300

    add 8.2.1

---
+++ 08-JUnit-and-Functional/src/main/java/com/leevilaune/palindrome/PalindromeChecker.java
@@ -0,0 +1,24 @@
+package com.leevilaune.palindrome;
+
+public class PalindromeChecker {
+
+    public PalindromeChecker(){
+
+    }
+
+    public boolean isPalindrome(String word){
+        if(word == null || word.isEmpty()){
+            return false;
+        }
+        word = word.toLowerCase();
+        word = word.replaceAll("[^a-z0-9]", "");
+        for(int i = 0; i<=word.length()/2;i++){
+            char c = word.charAt((word.length() - 1) - i);
+            System.out.printf("%s | %s\n",word.charAt(i), c);
+            if(word.charAt(i) != c){
+                return false;
+            }
+        }
+        return true;
+    }
+}
